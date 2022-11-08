79bb37c4a2a34ba859e0442d72188eca0d17ef89
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-08 09:00:12 +0600

    create firebase config with environment variable

---
+++ src/Pages/index.js
@@ -0,0 +1,11 @@
+import AddService from "./AddService/AddService";
+import Login from "./Authentication/Login/Login";
+import Register from "./Authentication/Register/Register";
+import Blogs from "./Blogs/Blogs/Blogs";
+import Home from "./Home/Home/Home";
+import MyReviews from "./MyReviews/MyReviews";
+import Profile from "./Profile/Profile";
+import ServiceDetails from "./ServiceDetails/ServiceDetails";
+import Services from "./Services/Services/Services";
+
+export { Home, Blogs, Services, AddService, Login, Register, MyReviews, Profile, ServiceDetails };
