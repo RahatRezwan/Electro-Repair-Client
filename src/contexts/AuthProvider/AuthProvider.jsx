dc10d711cee49a5bad439343df1cba909013a8c9
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-08 10:29:09 +0600

    create responsive navbar using flowbite

--- src/contexts/AuthProvider/AuthProvider.jsx
+++ src/contexts/AuthProvider/AuthProvider.jsx
@@ -1,10 +1,20 @@
 import React, { createContext, useState } from "react";
 import app from "../../firebase/firebase.config";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+   createUserWithEmailAndPassword,
+   getAuth,
+   GoogleAuthProvider,
+   signInWithEmailAndPassword,
+   signInWithPopup,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
 
 const auth = getAuth(app);
+
+/* Google Auth provider */
+const googleProvider = new GoogleAuthProvider();
+
 const AuthProvider = ({ children }) => {
    const [user, setUser] = useState(null);
    const [loader, setLoader] = useState(true);
@@ -21,8 +31,15 @@ const AuthProvider = ({ children }) => {
       return signInWithEmailAndPassword(auth, email, password);
    };
 
-   /*  */
-   return <AuthContext.Provider></AuthContext.Provider>;
+   /* Google Login */
+   const googleLogin = () => {
+      setLoader(true);
+      return signInWithPopup(auth, googleProvider);
+   };
+
+   const authInfo = { user, loader, createANewUser, loginAUser, googleLogin };
+
+   return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
--- src/routes/routes/routes.js
+++ src/routes/routes/routes.js
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
+import { Home } from "../../Pages";
 
 export const routes = createBrowserRouter([
    {
       path: "/",
       element: <Main />,
+      children: [
+         {
+            path: "/",
+            element: <Home />,
+         },
+      ],
    },
 ]);
