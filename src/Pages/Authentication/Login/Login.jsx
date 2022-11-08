a9bae05965c8e72b3ea38c3f48424894c1d32e62
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-08 12:35:27 +0600

    create login page

--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -1,45 +1,78 @@
-import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import { Button, Checkbox, Dropdown, Label, TextInput } from "flowbite-react";
 import React from "react";
+import { GoMail, GoKey } from "react-icons/go";
+import { FaGoogle, FaFacebook } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import loginImg from "../../../assests/loginpage.png";
 
 const Login = () => {
    return (
-      <div className="min-h-screen max-w-lg mx-auto mt-8">
-         <form className="flex flex-col gap-4 ">
-            <div>
-               <div className="mb-2 block">
-                  <Label htmlFor="email2" value="Your email" />
+      <div className="min-h-screen w-full mx-auto mt-8 flex flex-cols lg:flex-row justify-evenly items-center">
+         <div className="w-full flex justify-center">
+            <img src={loginImg} alt="" className="w-50" />
+         </div>
+         <div className="w-full flex justify-center">
+            <form className="flex flex-col gap-4 border border-gray-300 rounded-md p-[50px] w-[80%]">
+               <div>
+                  <h1 className="text-3xl text-center">Login</h1>
                </div>
-               <TextInput
-                  id="email2"
-                  type="email"
-                  placeholder="name@flowbite.com"
-                  required={true}
-                  shadow={true}
-               />
-            </div>
-            <div>
-               <div className="mb-2 block">
-                  <Label htmlFor="password2" value="Your password" />
+               <div>
+                  <div className="mb-2 block">
+                     <Label htmlFor="email2" value="Your email" />
+                  </div>
+                  <TextInput
+                     id="email2"
+                     type="email"
+                     placeholder="example@email.com"
+                     required={true}
+                     shadow={true}
+                     icon={GoMail}
+                  />
                </div>
-               <TextInput id="password2" type="password" required={true} shadow={true} />
-            </div>
-            <div>
-               <div className="mb-2 block">
-                  <Label htmlFor="repeat-password" value="Repeat password" />
+               <div>
+                  <div className="mb-2 block">
+                     <Label htmlFor="password2" value="Your password" />
+                  </div>
+                  <TextInput
+                     id="password2"
+                     type="password"
+                     placeholder="your password"
+                     required={true}
+                     shadow={true}
+                     icon={GoKey}
+                  />
                </div>
-               <TextInput id="repeat-password" type="password" required={true} shadow={true} />
-            </div>
-            <div className="flex items-center gap-2">
-               <Checkbox id="agree" />
-               <Label htmlFor="agree">
-                  I agree with the{" "}
-                  <a href="/forms" className="text-blue-600 hover:underline dark:text-blue-500">
-                     terms and conditions
-                  </a>
-               </Label>
-            </div>
-            <Button type="submit">Register new account</Button>
-         </form>
+               <div className="text-center hover:text-blue-700">
+                  <Link>
+                     <p>Forgot Password?</p>
+                  </Link>
+               </div>
+               <Button type="submit">Login</Button>
+               <div>
+                  <p className="text-center">
+                     New to this website?{" "}
+                     <Link to="/register">
+                        <span className="text-blue-700">Register</span>
+                     </Link>
+                  </p>
+               </div>
+               <div className="flex justify-between items-center">
+                  <hr className="bg-gray-600 border-none h-[1px] rounded-lg w-[40%]" />
+                  <p>OR</p>
+                  <hr className="bg-gray-600 border-none h-[1px] rounded-lg w-[40%]" />
+               </div>
+
+               <p className="text-center">Login With</p>
+               <div className="grid grid-cols-2 gap-3">
+                  <Button type="submit">
+                     <FaGoogle className="h-5 w-5 mr-2" /> Google
+                  </Button>
+                  <Button type="submit">
+                     <FaFacebook className="h-5 w-5 mr-2" /> Facebook
+                  </Button>
+               </div>
+            </form>
+         </div>
       </div>
    );
 };
--- src/Pages/Authentication/Register/Register.jsx
+++ src/Pages/Authentication/Register/Register.jsx
@@ -1,7 +1,59 @@
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
 import React from "react";
+import { GoKey, GoMail } from "react-icons/go";
 
 const Register = () => {
-   return <div>Register</div>;
+   return (
+      <div className="min-h-screen max-w-lg mx-auto mt-8">
+         <form className="flex flex-col gap-4 border border-gray-300 rounded-md p-[10px]">
+            <div>
+               <h1 className="text-3xl text-center">Login</h1>
+            </div>
+            <div>
+               <div className="mb-2 block">
+                  <Label htmlFor="email2" value="Your email" />
+               </div>
+               <TextInput
+                  id="email2"
+                  type="email"
+                  placeholder="example@email.com"
+                  required={true}
+                  shadow={true}
+                  icon={GoMail}
+               />
+            </div>
+            <div>
+               <div className="mb-2 block">
+                  <Label htmlFor="password2" value="Your password" />
+               </div>
+               <TextInput
+                  id="password2"
+                  type="password"
+                  placeholder="your password"
+                  required={true}
+                  shadow={true}
+                  icon={GoKey}
+               />
+            </div>
+            <div>
+               <div className="mb-2 block">
+                  <Label htmlFor="repeat-password" value="Repeat password" />
+               </div>
+               <TextInput id="repeat-password" type="password" required={true} shadow={true} />
+            </div>
+            <div className="flex items-center gap-2">
+               <Checkbox id="agree" />
+               <Label htmlFor="agree">
+                  I agree with the{" "}
+                  <a href="/forms" className="text-blue-600 hover:underline dark:text-blue-500">
+                     terms and conditions
+                  </a>
+               </Label>
+            </div>
+            <Button type="submit">Login</Button>
+         </form>
+      </div>
+   );
 };
 
 export default Register;
