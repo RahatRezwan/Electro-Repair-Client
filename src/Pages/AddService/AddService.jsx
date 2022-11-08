a2a6d23a3daadcf5a054706b6e7efdeb0304cafc
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-08 20:01:38 +0600

    create fetch for post a service to the server

--- src/Pages/AddService/AddService.jsx
+++ src/Pages/AddService/AddService.jsx
@@ -1,12 +1,39 @@
-import { Button, Checkbox, FileInput, Label, Textarea, TextInput } from "flowbite-react";
+import { Button, Label, Textarea, TextInput } from "flowbite-react";
 import React from "react";
-
 const AddService = () => {
+   const handleAddService = (event) => {
+      event.preventDefault();
+      const form = event.target;
+
+      const title = form.title.value;
+      const price = form.price.value;
+      const duration = form.duration.value;
+      const imgUrl = form.imgUrl.value;
+      const description = form.description.value;
+
+      const service = { title, price, duration, imgUrl, description };
+      fetch("http://localhost:5000/services", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify(service),
+      })
+         .then((res) => res.json())
+         .then((data) => {
+            if (data.acknowledged) {
+               alert("Product Added Successfully");
+               form.reset();
+               console.log(data);
+            }
+         });
+   };
+
    return (
       <div className="max-w-[1320px] mx-auto mt-10 pt-10 min-h-screen">
-         <div className="border rounded-md shadow-md p-5 w-[80%] mx-auto ">
+         <div className="border rounded-md shadow-md p-7 w-[80%] mx-auto ">
             <h1 className="text-3xl font-extrabold mb-5">Create A New Service</h1>
-            <form className="flex flex-col gap-4">
+            <form onSubmit={handleAddService} className="flex flex-col gap-4">
                <div>
                   <div className="mb-2 block">
                      <Label htmlFor="title" value="Enter Service Title" />
@@ -14,6 +41,7 @@ const AddService = () => {
                   <TextInput
                      id="title"
                      type="text"
+                     name="title"
                      placeholder="Service Title"
                      required={true}
                      shadow={true}
@@ -26,6 +54,7 @@ const AddService = () => {
                   <TextInput
                      id="price"
                      type="text"
+                     name="price"
                      placeholder="Price"
                      required={true}
                      shadow={true}
@@ -33,21 +62,29 @@ const AddService = () => {
                </div>
                <div>
                   <div className="mb-2 block">
-                     <Label htmlFor="Duration" value="Duration" />
+                     <Label htmlFor="duration" value="duration" />
                   </div>
                   <TextInput
-                     id="Duration"
+                     id="duration"
                      type="text"
+                     name="duration"
                      placeholder="Duration"
                      required={true}
                      shadow={true}
                   />
                </div>
-               <div id="fileUpload">
+               <div>
                   <div className="mb-2 block">
-                     <Label htmlFor="file" value="Upload Cover Image" />
+                     <Label htmlFor="imgUrl" value="Cover Image Url" />
                   </div>
-                  <FileInput id="file" />
+                  <TextInput
+                     id="imgUrl"
+                     type="text"
+                     name="imgUrl"
+                     placeholder="imgUrl"
+                     required={true}
+                     shadow={true}
+                  />
                </div>
 
                <div id="textarea">
@@ -56,9 +93,10 @@ const AddService = () => {
                   </div>
                   <Textarea
                      id="description"
+                     name="description"
                      placeholder="Service description..."
                      required={true}
-                     rows={4}
+                     rows={7}
                   />
                </div>
 
